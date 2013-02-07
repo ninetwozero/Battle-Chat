1f00bb0df17f7807ee98754cb2e3bfc93d379caf
Karl Lindmark <support@ninetwozero.com>
2013-02-07 21:54:27 +0100

    Added the needed activities to make this work.

--- src/com/ninetwozero/battlechat/ChatActivity.java
+++ src/com/ninetwozero/battlechat/ChatActivity.java
@@ -1,29 +1,21 @@
 package com.ninetwozero.battlechat;
 
 import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.ListView;
 
-public class MainActivity extends ListActivity {
+public class ChatActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_chat);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
-	}
-	
-	@Override
-	protected void onListItemClick(ListView listView, View view, int position, long id) {
-		startActivity( new Intent(this, ChatActivity.class).putExtra("profileId", id) );
 	}
 
 }
