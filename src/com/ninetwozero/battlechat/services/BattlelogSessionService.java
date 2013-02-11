54ee7f43b3c2008538e4ed46414fea916425f4e5
Karl Lindmark <karl.lindmark@ninetwozero.com>
2013-02-11 19:44:43 +0100

    Starting to work on the SessionService

---
+++ src/com/ninetwozero/battlechat/services/BattlelogSessionService.java
@@ -0,0 +1,19 @@
+package com.ninetwozero.battlechat.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BattlelogSessionService extends Service {
+
+	public BattlelogSessionService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
