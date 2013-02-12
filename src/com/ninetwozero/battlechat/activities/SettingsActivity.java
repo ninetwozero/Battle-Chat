fa1dd0c05a6ae2c9a8321cd5b89fc2d263546c33
Karl Lindmark <support@ninetwozero.com>
2013-02-12 01:23:39 +0100

    Adding a placeholder activity for the Settings. WIP. Heading to bed now\!

---
+++ src/com/ninetwozero/battlechat/activities/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.ninetwozero.battlechat.activities;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/* TODO: 
+ * Create preferences in xml format
+ * Add preferences from xml 
+ * */
+
+public class SettingsActivity extends PreferenceActivity {
+
+	public static final String TAG = "SettingsActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
